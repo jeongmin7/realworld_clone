b204a185a1d0dff75211b3fc2ab94074197fb2e6
jeongmin <lee.jeongmin1625@gmail.com>
2023-02-13 00:19:06 +0900

    Feat: router 설정 및 realworld css 복사

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Settings from './pages/Settings';
+import Create from './pages/Create';
+import Article from './pages/Article';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return <div className="App">초기설정</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/editor" element={<Create />} />
+        <Route path="/signin" element={<Register />} />
+        <Route path="/article/:id" element={<Article />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
